45575eecfaabeb92829b8a8e992f544aeeb328a5
taylorperkins <taylorperkins.dev@gmail.com>
2018-07-08 14:17:50 -0500

    Basic html template together. I will be bringing in a free bootstrap template to work off of next.

--- a_juicy_project/views/hello_world.py
+++ a_juicy_project/views/hello_world.py
@@ -1,6 +1,8 @@
+from flask import render_template
+
 from a_juicy_project import app
 
 
-@app.route('/hello_world')
-def hello_world():
-    return 'Hello World! :)'
+@app.route('/hello_world/<name>')
+def hello_world(name):
+    return render_template('hello_world.html', name=name)
